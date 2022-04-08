b47aa7f1a26056e16e0c63d5007bef92163345cd
Ayush Patel <79853695+ayush-patel-78@users.noreply.github.com>
2022-04-08 20:20:34 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: 'Ayush', email: 'Ayush@test.com' },
+  { name: 'Huehue', email: 'huehue@test.com' },
+  { name: 'hehe', email: 'heheh@test.com' },
+  { name: 'SAM', email: 'sam@test.com' },
+]
+module.exports = data
