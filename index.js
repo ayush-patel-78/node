53753229c961f478f19e4c6be33d7603324c3710
Ayush <ayushkurmi97@gmail.com>
2022-04-09 10:54:55 +0530

    creating 404 page

--- index.js
+++ index.js
@@ -118,5 +118,21 @@ const app = express()
 const path = require('path')
 
 const publicPath = path.join(__dirname, 'public')
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+app.get('/', (req, resp) => {
+  resp.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (req, resp) => {
+  resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help', (req, resp) => {
+  resp.sendFile(`${publicPath}/help.html`)
+})
+
+app.get('*', (req, resp) => {
+  resp.sendFile(`${publicPath}/nopagefound.html`)
+})
 app.listen(3000)
