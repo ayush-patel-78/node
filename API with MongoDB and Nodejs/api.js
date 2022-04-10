f0699b7d52c472d1cd2e87a188d56ee42bc8df70
Ayush <ayushkurmi97@gmail.com>
2022-04-10 12:20:39 +0530

    POST method API in nodejs

--- API with MongoDB and Nodejs/api.js
+++ API with MongoDB and Nodejs/api.js
@@ -2,10 +2,23 @@ const express = require('express')
 const dbConnect = require('../mongodb')
 const app = express()
 
+app.use(express.json())
+
+// GET Api method
+
 app.get('/', async (req, resp) => {
   const db = await dbConnect()
   const data = await db.find().toArray()
   console.log(data)
+  resp.send(data)
+})
+
+// POST API method
+
+app.post('/', async (req, resp) => {
+  console.log(req.body)
+  const db = dbConnect()
+  const data = await db.insertMany(req.body)
   resp.send(data)
 })
 app.listen(3000)
